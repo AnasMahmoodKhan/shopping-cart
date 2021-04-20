950f4dfc6f5ba6d9a7c4ec39518c55e219df790a
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-04-20 21:33:05 +0530

    Added items list to Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = ({ items }) => {
+  const handleClick = (id) => {
+    console.log(id);
+  };
+
+  return (
+    <div className="container">
+      <h3 className="center">Our items</h3>
+      <div className="box">
+        {items.map((item) => (
+          <div className="card" key={item.id}>
+            <div className="card-image">
+              <img src={item.img} alt={item.title} />
+              <span className="card-title">{item.title}</span>
+              <span
+                to="/"
+                className="btn-floating halfway-fab waves-effect waves-light red"
+                onClick={() => {
+                  handleClick(item.id);
+                }}
+              >
+                <i className="material-icons">add</i>
+              </span>
+            </div>
+
+            <div className="card-content">
+              <p>{item.desc}</p>
+              <p>
+                <b>Price: {item.price}$</b>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    items: state.items,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+Home.defaultProps = {
+  items: [],
+};
+
+Home.propTypes = {
+  items: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
