405fe42a7f973b28a3268b749885efcc209242a0
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-04-21 14:33:49 +0530

    Added tests for Home component (Home.js)

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,34 +8,38 @@ const Home = ({ items }) => {
   };
 
   return (
-    <div className="container">
+    <div className="container" data-test="Home">
       <h3 className="center">Our items</h3>
-      <div className="box">
-        {items.map((item) => (
-          <div className="card" key={item.id}>
-            <div className="card-image">
-              <img src={item.img} alt={item.title} />
-              <span className="card-title">{item.title}</span>
-              <span
-                to="/"
-                className="btn-floating halfway-fab waves-effect waves-light red"
-                onClick={() => {
-                  handleClick(item.id);
-                }}
-              >
-                <i className="material-icons">add</i>
-              </span>
-            </div>
+      {items.length > 0 ? (
+        <div className="box" data-test="box">
+          {items.map((item) => (
+            <div className="card" data-test="card" key={item.id}>
+              <div className="card-image">
+                <img src={item.img} alt={item.title} />
+                <span className="card-title">{item.title}</span>
+                <span
+                  to="/"
+                  className="btn-floating halfway-fab waves-effect waves-light red"
+                  onClick={() => {
+                    handleClick(item.id);
+                  }}
+                >
+                  <i className="material-icons">add</i>
+                </span>
+              </div>
 
-            <div className="card-content">
-              <p>{item.desc}</p>
-              <p>
-                <b>Price: {item.price}$</b>
-              </p>
+              <div className="card-content">
+                <p>{item.desc}</p>
+                <p>
+                  <b>Price: {item.price}$</b>
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <span data-test="noitems">No items found!</span>
+      )}
     </div>
   );
 };
